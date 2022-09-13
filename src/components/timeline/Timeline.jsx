a1a104662390af8c9a3cda00eff6491d95b1402a
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-09-13 13:16:22 +0900

    Postの雛形を作成

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Timeline.css';
 import Share from '../share/Share'
+import Post from '../post/Post'
 
 function Timeline() {
     return (
         <div className='timeline'>
             <div className="timelineWrapper">
                 <Share />
-                {/* <Post /> */}
+                <Post />
             </div>
         </div>
     )

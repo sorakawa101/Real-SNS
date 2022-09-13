38ca01d3d079b076305f417721a122ee19011fd8
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-09-14 00:39:25 +0900

    add:PostをCSSでスタイリング

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -7,12 +7,14 @@ export default function Post() {
         <div className='post'>
             <div className="postWrapper">
                 <div className="postTop">
-                    <img src='./assets/person/1.jpeg' alt='' className='postProfileImg' />
-                    <span className='postUsername'>Sorakawa101</span>
-                    <span className="postDate">5分前</span>
-                </div>
-                <div className="postRight">
-                    <MoreVert />
+                    <div className="postTopLeft">
+                        <img src='./assets/person/1.jpeg' alt='' className='postProfileImg' />
+                        <span className='postUsername'>Sorakawa101</span>
+                        <span className="postDate">5分前</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
                 </div>
                 <div className="postCenter">
                     <span className="postText">SNSを自作中です</span>

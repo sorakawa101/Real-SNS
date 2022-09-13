6a18994ad98372c773729e3f5505290dc74ce978
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-09-14 00:57:48 +0900

    RightbarをCSSでスタイリング

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -11,15 +11,22 @@ function Rightbar() {
                         <b>フォロワー限定</b>イベント開催中！
                     </span>
                 </div>
-                <img src='./assets/event.jpeg' alt='' />
+                <img src='./assets/event.jpeg' alt='' className='eventImg' />
                 <h4 className='rightbarTitle'>オンラインの友達</h4>
                 <ul className="rightbarFriendList">
                     <li className="rightbarFriend">
                         <div className="rightbarProfileImgContainer">
-                            <img src='./assets/person/1.jpeg' alt='' />
+                            <img src='./assets/person/1.jpeg' alt='' className='rightbarProfileImg'/>
                             <span className="rightbarOnline"></span>
                         </div>
                         <span className="rightbarUsername">Sorakawa101</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src='./assets/person/2.jpeg' alt='' className='rightbarProfileImg' />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Tanaka</span>
                     </li>
                 </ul>
             </div>
